ecc1ebd13bf40a1fbbd81f3bb583b6a10005aacb
Dan.Teng <engxiangfei@gmail.com>
2020-06-28 16:56:40 +0800

    add Homepage ListView

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 
 class Homepage extends StatefulWidget {
@@ -8,29 +9,48 @@ class Homepage extends StatefulWidget {
     return _HomepageState();
   }
 
-  List a = _getDatas();
+  // var a = _getDatas();
 }
 
 class _HomepageState extends State {
+  List _dataList = List();
+
+  @override
+  void initState() {
+    super.initState();
+    this._getDatas();
+  }
+
+  _getDatas() async {
+    Dio dio = new Dio();
+    Response res = await dio.get(
+        'https://itunes.apple.com/cn/rss/topfreeapplications/limit=100/json');
+    if (res.statusCode == 200) {
+      // print(jsonDecode(res.data));
+      setState(() {
+        _dataList = jsonDecode(res.data)['feed']['entry'];
+      });
+    } else {
+      print('Error Code:${res.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('HomePage'),
+        title: Text('Homepage'),
       ),
-      body: Center(
-        child: Text('home'),
+      body: ListView.builder(
+        itemCount: _dataList.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            leading: Image.network(_dataList[index]['im:image'][0]['label']),
+            title: Text(_dataList[index]['im:name']['label']),
+            subtitle: Text(_dataList[index]['category']['attributes']['label']),
+          );
+        },
       ),
     );
   }
-}
-
-_getDatas() async {
-  List dataList = List();
-
-  Dio dio = new Dio();
-  Response res = await dio.get(
-      'https://itunes.apple.com/cn/rss/topfreeapplications/limit=100/json');
-  print(res.data.toString());
-  return dataList;
 }
