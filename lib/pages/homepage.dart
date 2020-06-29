08171dceae5efc52164f44f2f8d4f19891ac1093
Dan.Teng <engxiangfei@gmail.com>
2020-06-29 12:53:38 +0800

    in progress

--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -8,8 +8,6 @@ class Homepage extends StatefulWidget {
   State<StatefulWidget> createState() {
     return _HomepageState();
   }
-
-  // var a = _getDatas();
 }
 
 class _HomepageState extends State {
@@ -41,15 +39,88 @@ class _HomepageState extends State {
       appBar: AppBar(
         title: Text('Homepage'),
       ),
-      body: ListView.builder(
-        itemCount: _dataList.length,
-        itemBuilder: (BuildContext context, int index) {
-          return ListTile(
-            leading: Image.network(_dataList[index]['im:image'][0]['label']),
-            title: Text(_dataList[index]['im:name']['label']),
-            subtitle: Text(_dataList[index]['category']['attributes']['label']),
-          );
-        },
+      body: this._dataList.length == 0
+          ? Center(
+              child: Text(
+                '加载中...',
+                style: TextStyle(fontSize: 16),
+              ),
+            )
+          : ListView.builder(
+              itemCount: _dataList.length,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  child: ListTile(
+                    leading:
+                        Image.network(_dataList[index]['im:image'][1]['label']),
+                    title: Text(
+                      _dataList[index]['im:name']['label'],
+                      style: TextStyle(color: Color.fromRGBO(31, 86, 30, 1)),
+                    ),
+                    subtitle: Text(
+                      _dataList[index]['category']['attributes']['label'],
+                      style: TextStyle(color: Color.fromRGBO(142, 133, 16, 1)),
+                    ),
+                    onTap: () => _tapRowWithData(context, index),
+                  ),
+                  decoration: BoxDecoration(
+                      border: Border(
+                          bottom:
+                              BorderSide(width: 1, color: Color(0xffe5e5e5))),
+                      color: Colors.white),
+                );
+              },
+            ),
+    );
+  }
+
+  //异步处理
+  _tapRowWithData(BuildContext context, int index) async {
+    final result = await Navigator.push(
+        context,
+        MaterialPageRoute(
+            builder: (context) =>
+                ProductDetailPage(appIterm: _dataList[index])));
+    if (null != result) {
+      //第一页面提示返回过来的值
+      Scaffold.of(context).showSnackBar(SnackBar(content: Text('$result')));
+    }
+  }
+}
+
+class ProductDetailPage extends StatelessWidget {
+  final Map appIterm;
+  ProductDetailPage({Key key, @required this.appIterm}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(appIterm['im:name']['label'])),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 15,
+          ),
+          Center(child: Image.network(appIterm['im:image'][2]['label'])),
+          Container(
+            height: 15,
+          ),
+          Center(
+              child: Text(appIterm['title']['label'],
+                  style: TextStyle(fontSize: 15))),
+          Container(
+            height: 15,
+          ),
+          Container(
+            height: 15,
+          ),
+          RaisedButton(
+              onPressed: () {
+                Navigator.pop(context, 'product.description');
+              },
+              child: Text('返回'),
+              color: Colors.purple)
+        ],
       ),
     );
   }
