e6a05ebf50ebf851cefec0578db814c099cde740
Dan.Teng <engxiangfei@gmail.com>
2020-06-29 21:45:33 +0800

    show detail page

--- lib/pages/detail_page.dart
+++ lib/pages/detail_page.dart
@@ -17,37 +17,66 @@ class _DetailPageState extends State<DetailPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-          title: Text(
-        this.appInfo['im:name']['label'],
-        style: TextStyle(fontSize: 14, fontWeight: FontWeight.w300),
-      )),
-      body: Column(
-        children: <Widget>[
-          Container(
-            height: 15,
-          ),
-          Center(child: Image.network(appInfo['im:image'][2]['label'])),
-          Container(
-            height: 15,
-          ),
-          Center(
-              child: Text(appInfo['title']['label'],
-                  style: TextStyle(fontSize: 15))),
-          Container(
-            height: 15,
-          ),
-          Container(
-            height: 15,
-          ),
-          RaisedButton(
-              onPressed: () {
-                Navigator.pop(context, 'product.description');
-              },
-              child: Text('返回'),
-              color: Colors.purple)
-        ],
-      ),
-    );
+        appBar: AppBar(
+            title: Text(
+          this.appInfo['im:name']['label'],
+          style: TextStyle(fontSize: 15, fontWeight: FontWeight.w300),
+        )),
+        body: CustomScrollView(
+          shrinkWrap: true,
+          slivers: <Widget>[
+            SliverPadding(
+              padding: const EdgeInsets.all(20.0),
+              sliver: new SliverList(
+                delegate: new SliverChildListDelegate(
+                  <Widget>[
+                    Center(
+                        child: Image.network(appInfo['im:image'][2]['label'])),
+                    Container(
+                      height: 15,
+                    ),
+                    Center(
+                      child: Row(
+                        children: <Widget>[
+                          Text(appInfo['im:name']['label'],
+                              style: TextStyle(fontSize: 15)),
+                          Text(
+                              ' - [${appInfo['category']['attributes']['label']}]',
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.blue))
+                        ],
+                        mainAxisAlignment: MainAxisAlignment.center,
+                      ),
+                    ),
+                    Container(
+                      height: 15,
+                    ),
+                    Center(
+                        child: Text('From: ${appInfo['rights']['label']}',
+                            style:
+                                TextStyle(fontSize: 14, color: Colors.orange))),
+                    Container(
+                      height: 15,
+                    ),
+                    Center(
+                        child: Text('${appInfo['summary']['label']}',
+                            style: TextStyle(
+                              fontSize: 14,
+                            ))),
+                    Container(
+                      height: 15,
+                    ),
+                    RaisedButton(
+                        onPressed: () {
+                          Navigator.pop(context, 'product.description');
+                        },
+                        child: Text('返回'),
+                        color: Colors.purple)
+                  ],
+                ),
+              ),
+            )
+          ],
+        ));
   }
 }
