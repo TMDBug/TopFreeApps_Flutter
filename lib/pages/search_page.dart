1a6c47f998ca3c2a2b8ed7dab91cefc9b8a967ea
Dan.Teng <engxiangfei@gmail.com>
2020-07-03 23:39:50 +0800

    improve search

--- lib/pages/search_page.dart
+++ lib/pages/search_page.dart
@@ -14,6 +14,8 @@ class _SearchPageState extends State<SearchPage>
   @override
   bool get wantKeepAlive => true;
 
+  List _dataList = [];
+
   @override
   Widget build(BuildContext context) {
     super.build(context);
@@ -25,22 +27,23 @@ class _SearchPageState extends State<SearchPage>
         },
       ),
       body: Center(
-        // child: Text(
-        //   '- Empty -',
-        //   style: TextStyle(
-        //       color: Color.fromRGBO(188, 188, 188, 1),
-        //       fontWeight: FontWeight.w200),
-        // ),
-        child: ListView.builder(
-          itemCount: 20,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              leading: Icon(Icons.search),
-              title: Text('APP序号： $index'),
-              subtitle: Text('多思考组件的合并 形成新的组件'),
-            );
-          },
-        ),
+        child: _dataList.length == 0
+            ? Text(
+                '- Empty -',
+                style: TextStyle(
+                    color: Color.fromRGBO(188, 188, 188, 1),
+                    fontWeight: FontWeight.w200),
+              )
+            : ListView.builder(
+                itemCount: 20,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    leading: Icon(Icons.search),
+                    title: Text('APP序号： $index'),
+                    subtitle: Text('多思考组件的合并 形成新的组件'),
+                  );
+                },
+              ),
       ),
     );
   }
